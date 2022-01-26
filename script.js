fcabef7df89be91f890c817a98027a23bffda6ef
Versantos <veronicasilvamagalhaes@gmail.com>
2022-01-25 23:58:56 -0300

    Desafio 1 feito

---
+++ script.js
@@ -0,0 +1,28 @@
+let nome = "Veronica";
+console.log("Veronica");
+
+
+let number="30";
+console.log("30");
+
+
+let number1 = 2022;
+let number2 = 30;
+let diminuir = ('number1 - number2');
+console.log("1992");
+
+
+let number3 = 2022;
+let number4 = 30;
+let somar = ('number1 + number2');
+console.log("2032");
+
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = "true";
+if (quartaFeira == "true") {
+    console.log("Maravilha");
+}else {
+    console.log("Lascou não é!!");
+}
